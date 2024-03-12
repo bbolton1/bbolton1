20acc987ac26bc7826880d842bec45e0e46c6d4f
Brayden Bolton <157667096+bbolton1@users.noreply.github.com>
2024-03-12 15:29:20 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,40 @@
 function displayResults() {
     const answer = document.getElementById('answer').value;
-
-    if (answer === "Photo"){
-        
-    }
+    const output = document.getElementById('output');
     
-    const finalMessage = `The last half of your name reversed is: ${nameModified}, and if you were in the world of Dungeons and Dragons, you would be a: ${speciesFinal}.`
-    const message = document.createTextNode(finalMessage);
-    const output = document.getElementById('fullOutput');       // puts the text into a node to append it
-    textOutput.innerHTML = '';
-    textOutput.appendChild(message);
+    if (answer === "Photo"){
+        const img = document.createElement('img');
+        img.src = 'styles/Brayden.jpg'; 
+        img.alt = 'Brayden'; 
+        img.width = 400;
+        img.height = 500; 
 
-    const img = document.createElement('img');
-    img.src = speciesImages[speciesFinal];
-    img.alt = speciesFinal; 
+        output.appendChild(img); 
+        const imageOutput = document.getElementById('output');
+        imageOutput.innerHTML = ''; 
+        imageOutput.appendChild(img);   // lines 13-15 are mainly to keep the function from being able
+                                        // to infinitely append
+    }
 
-    img.width = 500;
-    img.height = 500; // keeps images from taking up half the screen
+    else if (answer === "Personal info"){
+        const finalMessage = "My name is Brayden Bolton and I am an undergraduate IT student at UNA. I am from Russellville, Alabama." +
+        "I plan to focus on work involving cybersecurity and systems analysis.";
+        const message = document.createTextNode(finalMessage);
+        const output = document.getElementById('output');      
+        output.innerHTML = '';
+        output.appendChild(message);
+    }
 
-    output.appendChild(img);
+    else if (answer === "Capabilities"){
+        const finalMessage = "I am proficient in using Google Cloud services, Windows Active Directory, and Virtual Machines." +
+        "The coding language I am best at properly utilizing is Python. I am still learning about the different nuances of a myriad of programs " +
+        "and other software. Although I am capable of using these programs at an intermediate level. These include, but are not limited to, JavaScript, C++, Java," +
+        "C++, Java, JavaScript, GDB, Nginx, and MySQL. If you have any questions, please let me know at bbolton1@una.edu . Thank you for visiting my page";
+        const message = document.createTextNode(finalMessage);
+        const output = document.getElementById('output');       
+        output.innerHTML = '';
+        output.appendChild(message);
+    }
 
-    const imageOutput = document.getElementById('imageOutput');
-    imageOutput.innerHTML = ''; 
-    imageOutput.appendChild(img);
+
 }
