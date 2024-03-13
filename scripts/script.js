c37f19beca100754223813883fa208f5ed84f8a4
Brayden Bolton <157667096+bbolton1@users.noreply.github.com>
2024-03-13 15:30:49 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,13 @@
 function displayResults() {
     const answer = document.getElementById('answer').value;
     const output = document.getElementById('output');
-    
-    if (answer === "Photo"){
-        const img = document.createElement('img');
-        img.src = 'styles/Brayden.jpg'; 
-        img.alt = 'Brayden'; 
-        img.width = 400;
-        img.height = 500; 
 
-        output.innerHTML = '';
-        output.appendChild(img); 
-    }
-
-    else if (answer === "Personal info"){
+    if (answer === "Personal info"){
         const finalMessage = "My name is Brayden Bolton and I am an undergraduate IT student at UNA. I am from Russellville, Alabama." +
-        "I plan to focus on working in cybersecurity and systems analysis oriented fields. I recently turned 20";
+        "I plan to focus on work involving cybersecurity and systems analysis. I recently (3/12/2024) turned 20 ";
 
         const message = document.createTextNode(finalMessage);
-   
+    
         output.innerHTML = '';
         output.appendChild(message);
     }
@@ -27,13 +16,37 @@ function displayResults() {
         const finalMessage = "I am proficient in using Google Cloud services, Windows Active Directory, and a myriad of programming languages." +
         "These languages are as follows: Python, C++, Java, JavaScript, and MySQL." +
         "The coding language I am best at properly utilizing is Python. I also have experience in creating diagrams through Lucid Charts." +
-        "If you have any questions, please let me know at bbolton1@una.edu . Thank you for visiting my page.";
-                
+        "If you have any questions, please let me know at bbolton1@una.edu . Thank you for visiting my page";
+                    
         const message = document.createTextNode(finalMessage);
-      
+        
         output.innerHTML = '';
         output.appendChild(message);
     }
 
+    else if (answer === "Links"){
+        const links = [
+        {text: "Github Repositories", url: "https://example.com/link1"},
+        {text: "Codepen", url: "https://example.com/link2"},
+        {text: "Alpha", url: "https://example.com/link3"},
+        {text: "Bravo", url: "https://example.com/link3"},
+        {text: "Charlie", url: "https://example.com/link3"},
+        {text: "Delta", url: "https://example.com/link3"},
+        {text: "Echo", url: "https://example.com/link3"},
+        ];
+
+        output.innerHTML = ''; // Clear any previous content
+
+        // Loops through each link and creates an anchor element to automate the process
+        links.forEach(link => {
+        const anchor = document.createElement('a');
+        anchor.href = link.url;
+        anchor.className = 'btn btn-secondary m-2';
+        anchor.textContent = link.text;
+        output.appendChild(anchor);
+        });
+
+
+    }
 
 }
